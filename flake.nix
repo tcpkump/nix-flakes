b3620f92212f72dfdfe3d2d0f2daf317487063c3
Garrett Leber <garrett@leberra.com>
2025-04-29 19:53:16 -0400

    fix: output should be a function

--- flake.nix
+++ flake.nix
@@ -1,29 +1,31 @@
 {
   description = "A collection of flake templates";
 
-  outputs = {
-    templates = {
+  outputs =
+    { self }:
+    {
+      templates = {
 
-      ansible = {
-        path = ./ansible;
-        description = "Nix flake for Ansible role and playbook development";
+        ansible = {
+          path = ./ansible;
+          description = "Nix flake for Ansible role and playbook development";
+        };
+
+        kubernetes = {
+          path = ./kubernetes;
+          description = "Development environment for kubernetes-managed infrastructure or modules";
+        };
+
+        opentofu = {
+          path = ./opentofu;
+          description = "Development environment for OpenTofu-managed infrastructure or modules";
+        };
+
+        terraform = {
+          path = ./terraform;
+          description = "Development environment for Terraform-managed infrastructure or modules";
+        };
+
       };
-
-      kubernetes = {
-        path = ./kubernetes;
-        description = "Development environment for kubernetes-managed infrastructure or modules";
-      };
-
-      opentofu = {
-        path = ./opentofu;
-        description = "Development environment for OpenTofu-managed infrastructure or modules";
-      };
-
-      terraform = {
-        path = ./terraform;
-        description = "Development environment for Terraform-managed infrastructure or modules";
-      };
-
     };
-  };
 }
